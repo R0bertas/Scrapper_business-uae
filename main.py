9e80bdf01d089c2fb478762a4de34a830dc19b5b
Robert <62176158+R0bertas@users.noreply.github.com>
2020-04-11 11:27:08 +0100

    Can go to another website

--- main.py
+++ main.py
@@ -12,33 +12,52 @@ filename = "customerDataDMCC.csv"
 f = open(filename, "w", encoding='utf-8')
 
 # adding header
-#f.write("ID, Name, Email")  ## THERE IS NO ID , you can create easily in excel
+f.write("ID, Name, Email")  ## THERE IS NO ID , you can create easily in excel
 
-#f.write( " Name, Email")
-#f.write("\n")
+f.write( " Name, Email")
+f.write("\n")
 
-# waiting 5sec for iframe to load
-
+# waiting 5sec for to load
 time.sleep(5)
 
-def infopage():
+# this is first page
+# http://business-uae.com/eng/biz/jewellers-4800?p=1
+
+url  = "http://business-uae.com/eng/biz/jewellers-4800?p="
+
+# this method gathers info from x page  , it has 20 companies in it
+def infopage(x):
 
     for i in range(2):
         try:
+            time.sleep(2)
             comp_name = driver.find_element_by_xpath("/html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span ").get_attribute("innerHTML")
             print(comp_name)
+
+            f.write(comp_name)
+
+            # goes to another page
             driver.find_element_by_xpath(  "/html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span ").click()
-            time.sleep(2)
         except:
+            f.write("-")
             print("couldn't find name")
+
+        f.write(",")
         try:
             time.sleep(2)
             #  email = driver.find_element_by_xpath("/html/body/div[5]/div/main/div[1]/section/div[1]/div[1]/div[3]/div[2]/table/tbody/tr[1]/td[2]/span").get_attribute("innerHTML")
             email = driver.find_element_by_xpath("//span[@itemprop='email']").get_attribute("innerHTML")
+            f.write(email)
             print(email)
         except:
+            f.write("-")
             print("no email")
 
+        f.write("\n")
+        # this will go to another pager  we create temp because we need to keep starting url  with empty p=
+        temp_url= url + str(x)
+        print(temp_url)
+        driver.get(temp_url)
 
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span
@@ -50,4 +69,5 @@ def infopage():
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[5]/div[1]/h2/a/span
 #
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[6]/div[1]/h2/a/span
-infopage()
+for i in range(1,3):
+    infopage(i)
