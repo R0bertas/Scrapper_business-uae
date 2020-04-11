140b68afeff85d5aa7862bb5cfd7b7398da1c253
Robert <62176158+R0bertas@users.noreply.github.com>
2020-04-11 19:53:31 +0100

    Added timer, there is scraper detector
    Trying to avoid it

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
+import  random
 driver = webdriver.Chrome(executable_path="chromedriver.exe")  ## can be firefox or any other browser
 
 driver.get("http://business-uae.com/eng/biz/jewellers-4800")
@@ -12,9 +12,9 @@ filename = "customerDataDMCC.csv"
 f = open(filename, "w", encoding='utf-8')
 
 # adding header
-f.write("ID, Name, Email")  ## THERE IS NO ID , you can create easily in excel
+#f.write("ID, Name, Email")  ## THERE IS NO ID , you can create easily in excel
 
-f.write( " Name, Email")
+f.write( "Name, Email")
 f.write("\n")
 
 # waiting 5sec for to load
@@ -27,24 +27,30 @@ url  = "http://business-uae.com/eng/biz/jewellers-4800?p="
 
 # this method gathers info from x page  , it has 20 companies in it
 def infopage(x):
-
-    for i in range(2):
+    print("which page one: " + str(x)  )
+    for i in range(1,24):
+        temp_url = url + str(x)
+        print(" check name" + str(i))
+        driver.get(temp_url)
+        if i== 2 or i==3:
+            continue
         try:
-            time.sleep(2)
-            comp_name = driver.find_element_by_xpath("/html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span ").get_attribute("innerHTML")
+            time.sleep(random.randint(4,15))
+            comp_name = driver.find_element_by_xpath("/html/body/div[5]/div/main/div/div[6]/div[1]/div[ "+ str(i) +"]/div[1]/h2/a/span ").get_attribute("innerHTML")
             print(comp_name)
 
             f.write(comp_name)
 
-            # goes to another page
-            driver.find_element_by_xpath(  "/html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span ").click()
+            # goes to another compnay page to get an email
+            driver.find_element_by_xpath(  "/html/body/div[5]/div/main/div/div[6]/div[1]/div[ "+ str(i) +"]/div[1]/h2/a/span ").click()
         except:
             f.write("-")
             print("couldn't find name")
 
         f.write(",")
+        time.sleep(random.randint(4,15))
         try:
-            time.sleep(2)
+
             #  email = driver.find_element_by_xpath("/html/body/div[5]/div/main/div[1]/section/div[1]/div[1]/div[3]/div[2]/table/tbody/tr[1]/td[2]/span").get_attribute("innerHTML")
             email = driver.find_element_by_xpath("//span[@itemprop='email']").get_attribute("innerHTML")
             f.write(email)
@@ -55,9 +61,7 @@ def infopage(x):
 
         f.write("\n")
         # this will go to another pager  we create temp because we need to keep starting url  with empty p=
-        temp_url= url + str(x)
-        print(temp_url)
-        driver.get(temp_url)
+        time.sleep(random.randint(4,15))
 
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[1]/div[1]/h2/a/span
@@ -69,5 +73,6 @@ def infopage(x):
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[5]/div[1]/h2/a/span
 #
 # /html/body/div[5]/div/main/div/div[6]/div[1]/div[6]/div[1]/h2/a/span
-for i in range(1,3):
+for i in range(1,12):
+    time.sleep(random.randint(4,15))
     infopage(i)
